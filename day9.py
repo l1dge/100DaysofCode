2c457aa83765bc166f1ddd2ed36e3627a91ea17d
l1dge <lcullip@gmail.com>
2021-04-26 23:14:54 +0100

    To deepcopy or not!

--- day9.py
+++ day9.py
@@ -4,23 +4,31 @@ items = [
     {"id": 3, "name": "book", "value": 20},
 ]
 
-import unittest
+# import unittest, copy
+import copy
+
 
 def duplicate_items(items):
-    return items[:]
+    clone = copy.deepcopy(items)
+    return clone[:]
 
 
-class unittest
-def test_change_copy_only():
-    items_copy = duplicate_items(items)
-    assert items == items_copy
+# class TestDuplicateMethods(unittest.TestCase):
+#     def test_change_copy_only(self):
+#         items_copy = duplicate_items(items)
+#         items_copy = items_copy.copy()
+#         assert items == items_copy
 
-    # modify the copy
-    items_copy[0]['name'] = 'macbook'
-    items_copy[1]['id'] = 4
-    items_copy[2]['value'] = 30
+#         # modify the copy
+#         items_copy[0]["name"] = "macbook"
+#         items_copy[1]["id"] = 4
+#         items_copy[2]["value"] = 30
 
-    # only copy should have been updated, check original items values
-    assert items[0]['name'] == 'laptop'
-    assert items[1]['id'] == 2
-    assert items[2]['value'] == 20
+#         # only copy should have been updated, check original items values
+#         assert items[0]["name"] == "laptop"
+#         assert items[1]["id"] == 2
+#         assert items[2]["value"] == 20
+
+
+# if __name__ == "__main__":
+#     unittest.main()
