554e7c9fd3c1d5bd4dd15c0273362a3aa51359c1
l1dge <40536277+l1dge@users.noreply.github.com>
2021-05-06 09:53:20 +0100

    An easy one to start the day

---
+++ day19.py
@@ -0,0 +1,14 @@
+import itertools as it
+
+names = "Tim Bob Julian Carmen Sofia Mike Kim Andre".split()
+locations = "DE ES AUS NL BR US".split()
+confirmed = [False, True, True, False, True]
+
+
+def get_attendees():
+    for participant in it.zip_longest(names, locations, confirmed, fillvalue="-"):
+        print(participant)
+
+
+if __name__ == "__main__":
+    get_attendees()
